0edaf510a6d62535e02b48b1f364140845895f1f
jeasonchan <439994526@qq.com>
2019-06-20 23:55:36 +0800

    # 2019年6月20日23:53:28 * 新建脚本package，放有用的工具脚本 * 完成第一个脚本，打印输出目录下的所有文件的全路径

---
+++ UsefulScripts/NoName.py
@@ -0,0 +1,17 @@
+# encoding=utf-8
+import os
+
+# https://blog.csdn.net/chengxuyuanyonghu/article/details/59486631 学习网址
+class FileTools(object):
+    def __init__(self):
+        pass
+
+    def printAllFileNames(self, inputPath):
+        inputPath = str(inputPath)
+        for root, dirs, files in os.walk(inputPath):
+            for name in files:
+                print(os.path.join(root, name))  # todo 写进文本文件中，对输入路径做校验
+
+
+if __name__ == "__main__":
+    FileTools().printAllFileNames(inputPath="C:\CRroot\desktop\SpringiA4_SourceCode")

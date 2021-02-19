2cc4fea40b2433d375bb5944f607ad65b0af03ec
jeasonchan <439994526@qq.com>
2021-02-19 18:01:52 +0800

    完成调用命令行的小工具

--- unknown_tool/main.py
+++ unknown_tool/main.py
@@ -1,5 +1,5 @@
-#coding=UTF-8
-#!/usr/bin/python3
+#!/usr/bin/python
+# coding=utf-8
 
 import os
 import sys
@@ -7,85 +7,338 @@ import argparse
 import subprocess
 
 
+class Const:
+    toolName = "jeason_tool"
+    toolVersion = "1.0"
+
+    peopleRanges = "peopleRanges"
+    ageRanges = "ageRanges"
+    unknownRange = "unknown"
+
+    start = "start"
+    end = "end"
+    queryCommandTemplate = "exe1 \"string_params\""
+    configCommandTemplate = "exe2 -a value={}"
+    EXIT_WITH_SUCCESS = "EXIT WITH SUCCESS"
+    EXIT_WITH_CANCEL = "EXIT WITH CANCEL"
+    EXIT_WITH_ERROR = "EXIT WITH ERROR"
+
+
+class ModifyType:
+    __enum = [Const.peopleRanges, Const.ageRanges, Const.unknownRange]
+
+    @staticmethod
+    def getModifyType(inputStr):
+        """
+
+        :param inputStr: str
+        :return: str
+        """
+        ret = inputStr
+        for eachType in ModifyType.__enum:
+            if eachType.lower() == inputStr.lower():
+                return eachType
+
+        return ret
+
+
+class Util:
+    @staticmethod
+    def trans2BinaryTuple(numberStr):
+        numberStr = str(numberStr).strip()
+        if (numberStr.isdigit()):
+            return (numberStr, numberStr)
+        else:
+            raise Exception("{} is not numeric".format(numberStr))
+
+    @staticmethod
+    def add_range(items, start, end):
+        """
+        add a new range
+
+        """
+
+        if len(items) == 0:
+            items = []
+            items.append({"start": str(start), "end": str(end)})
+            return items
+        idx = 0
+        for item in items:
+            istart = long(item["start"])
+            iend = long(item["end"])
+            if istart > end:
+                items.insert(idx, {"start": str(start), "end": str(end)})
+                break
+            idx = idx + 1
+        if idx == len(items):
+            items.append({"start": str(start), "end": str(end)})
+        idx = 0
+        iitem = []
+        while idx < len(items) - 1:
+            c_end = long(items[idx]["end"])
+            n_start = long(items[idx + 1]["start"])
+            if n_start - c_end <= 1:
+                d = {"start": items[idx]["start"], "end": items[idx + 1]["end"]}
+                items.pop(idx + 1)
+                items.pop(idx)
+                items.insert(idx, d)
+                continue
+            iitem.append(items[idx])
+            idx = idx + 1
+        if idx == len(items) - 1:
+            iitem.append(items[idx])
+
+        return iitem
+
+    @staticmethod
+    def remove_range(items, start, end):
+        """
+        remove a  range
+
+        """
+        if len(items) == 0:
+            print("range is empty")
+            exit(-1)
+
+        idx = 0
+        while idx < len(items):
+            istart = long(items[idx]["start"])
+            iend = long(items[idx]["end"])
+            if (start >= istart) and (end <= iend):
+                part1_end = start - 1
+                part2_start = end + 1
+                items.pop(idx)
+                i = idx
+                if part1_end >= istart:
+                    items.insert(i, {"start": str(istart), "end": str(part1_end)})
+                    i = i + 1
+                if iend >= part2_start:
+                    items.insert(i, {"start": str(part2_start), "end": str(iend)})
+                break
+            idx = idx + 1
+
+        return items
+
+
+class Logger:
+    """
+    用来记日志，但是发现python已经自带的日志记录模块了
+    """
+    def __int__(self):
+        pass
+
+    def append(self, content):
+        print(content)
+
+
+import json
+import types
+
+
+class MyRange:
+    def __init__(self, modifyType, commandStr):
+        self.__modifyType = ModifyType.getModifyType(modifyType)
+
+        # obj json string
+        self.__objectJsonStr = commandStr
+
+        self.__indexOfLeft = -1
+        self.__indexOfRight = -1
+
+        # list[dict]
+        self.__rangeList = []
+
+        self.__initRangeList()
+
+    def __initRangeList(self):
+        indexOfTarget = self.__objectJsonStr.find(self.__modifyType)
+        if indexOfTarget < 0:
+            raise Exception("No {} config found!".format(self.__modifyType))
+
+        self.__indexOfLeft = self.__objectJsonStr.find("[", indexOfTarget)
+        self.__indexOfRight = self.__objectJsonStr.find("]", indexOfTarget)
+        originalRangeStr = self.__objectJsonStr[self.__indexOfLeft:self.__indexOfRight + 1]
+        originalRange = json.loads(originalRangeStr)
+
+        for eachDict in originalRange:
+            self.__rangeList.append(eachDict)
+
+        # print(self.__rangeList)
+
+    def getModifyType(self):
+        return self.__modifyType
+
+    def getRangeList(self):
+        return self.__rangeList
+
+    def getCoveredRange(self, portStr):
+        ret = {}
+        for eachDict in self.__rangeList:
+            currentNumber = int(portStr)
+            leftNumber = int(eachDict[Const.start])
+            rightNumber = int(eachDict[Const.end])
+            if leftNumber <= currentNumber <= rightNumber:
+                ret = eachDict
+                break
+        return ret
+
+    def addRange(self, range):
+        """
+        add a point or a range
+        :param range:str|(str,str)
+        :return:None
+        """
+        rangeType = type(range)
+
+        if rangeType == types.TupleType:
+            self.__addRange(range)
+        elif rangeType == types.StringType:
+            self.__addRange(Util.trans2BinaryTuple(range))
+        else:
+            raise Exception("unknown rangeType {}".format(rangeType))
+
+    def __addRange(self, rangeTuple):
+        """
+        add a range
+        :param rangeTuple:(str,str)
+        :return:None
+        """
+        # print(rangeTuple)
+        rangeTuple = tuple(rangeTuple)
+        self.__rangeList = Util.add_range(self.__rangeList, long(rangeTuple[0]), long(rangeTuple[1]))
+
+    def removeRange(self, range):
+        """
+       remove a point or a range
+       :param range:str|(str,str)
+       :return:None
+       """
+        rangeType = type(range)
+
+        if (rangeType == types.TupleType):
+            self.__removeRange(range)
+        elif (rangeType == types.StringType):
+            self.__removeRange(Util.trans2BinaryTuple(range))
+        else:
+            raise Exception("unknown rangeType {}".format(rangeType))
+
+    def __removeRange(self, rangeTuple):
+        """
+        remove a range
+        :param rangeTuple:(str,str)
+        :return:
+        """
+
+        rangeTuple = tuple(rangeTuple)
+        self.__rangeList = Util.remove_range(self.__rangeList, long(rangeTuple[0]), long(rangeTuple[1]))
+
+        pass
+
+    def getNewObjJsonStr(self):
+        return self.__objectJsonStr[0:self.__indexOfLeft] + \
+               json.dumps(self.__rangeList).replace(" ", "") + self.__objectJsonStr[self.__indexOfRight + 1:-1]
+
+
+# ==========================================================================================
+# ==========================================================================================
+# ==========================================================================================
+# ==========================================================================================
+# ==========================================================================================
+
 print("input argv is {}".format(sys.argv))
 
+parser = argparse.ArgumentParser(prog=Const.toolName,
+                                 description="add and remove {} and {}".format(Const.peopleRanges, Const.ageRanges))
+parser.add_argument("--version", "-v", action="version", version='%(prog)s {}'.format(Const.toolVersion))
+parser.add_argument('--type', '-t', action="store", choices=[Const.peopleRanges, Const.ageRanges], required=True)
+parser.add_argument('--add', '-a', nargs="+", action='store',
+                    help='add a range or single value or several values and ranges, such as: \n {} -a 123 456 ;\n {} -a 123,456 666,789 ;\n {} -a 123 456 666,789'.format(
+                        Const.toolName, Const.toolName, Const.toolName))
+parser.add_argument('--remove', '-r', nargs="+", action='store',
+                    help='remove a range or single value or several values and ranges, such as: \n {} -r 123 456 ;\n {} -r 123,456 666,789 ;\n {} -r 123 456 666,789'.format(
+                        Const.toolName, Const.toolName, Const.toolName))
 
-# description参数可以用于插入描述脚本用途的信息，可以为空
-parser = argparse.ArgumentParser(prog="unknown_tool",description="your script description")
-parser.add_argument("--version","-v", action="version", version='%(prog)s 1.0')
+parseredArgs = parser.parse_args()
 
-# 添加--verbose标签，标签别名可以为-v，这里action的意思是当读取的参数中出现--verbose/-v的时候
-# 参数字典的verbose建对应的值为True，而help参数用于描述--verbose参数的用途或意义。
-# 将变量以标签-值的字典形式存入args字典
-# 默认已经添加了--help和-h
-parser.add_argument('--add', '-a', action='append', help='add a range or single value')
-parser.add_argument('--remove', '-r', action='append', help='remove a range or single value')
+modifyTarget = parseredArgs.type
+print("type inputs:{}".format(modifyTarget))
 
-
-
-parseredArgs = parser.parse_args()        
-
-
-add_list=parseredArgs.add
-if(add_list):
+# such as ['123', '456,456']
+add_list = parseredArgs.add
+if add_list:
     print("add inputs:{}".format(add_list))
 
-remove_list=parseredArgs.remove
-if(remove_list):
+# such as ['123', '456,456']
+remove_list = parseredArgs.remove
+if remove_list:
     print("remove inputs:{}".format(remove_list))
 
+resultFile = None
+try:
 
-result = subprocess.call("cat /home/jeason/projects/pythonStudy/MainForFirstCommit.py",shell=True)
-print(result)
-"""
-输出：
-# encoding=utf-8
+    resultFile = os.popen(Const.queryCommandTemplate)
+    resultStrArray = resultFile.readlines()
+    print("query command result:\n{}".format(resultStrArray))
 
-if __name__ == "__main__":
-    print("hello world")
-0
+    if len(resultStrArray) < 3:
+        raise Exception("result too short!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-最后还带了一个0，表示主函数return 0这个返回值
+    result = resultStrArray[2]
 
-"""
+finally:
+    if resultFile:
+        resultFile.close()
 
-statusCode,output=subprocess.getstatusoutput("cat /home/jeason/projects/pythonStudy/MainForFirstCommit.py")
-print("statusCode:{}".format(statusCode))
-print(output)
-"""
-statusCode:0
-# encoding=utf-8
+indexOfObjStart = result.find("{")
+indexOfObjEnd = result.rfind("}")
+paramRange = MyRange(modifyTarget, result[indexOfObjStart:indexOfObjEnd + 1])
 
-if __name__ == "__main__":
-    print("hello world")
-"""
+print("{}\nfrom :\n{}".format(paramRange.getModifyType(), paramRange.getRangeList()))
+if add_list:
+    for index in range(0, len(add_list)):
+        each = add_list[index]
+        if each.find(",") < 0:
+            paramRange.addRange(each)
+        else:
+            tempArray = each.split(",")
+            paramRange.addRange((tempArray[0], tempArray[1]))
 
+if remove_list:
+    for index in range(0, len(remove_list)):
+        each = remove_list[index]
+        if each.find(",") < 0:
+            paramRange.removeRange(each)
+        else:
+            tempArray = each.split(",")
+            paramRange.removeRange((tempArray[0], tempArray[1]))
 
+print("to :\n{}".format(paramRange.getRangeList()))
+comfirm = raw_input("Continue with \"y\" or cancel with \"n\":")
 
-result=os.system("cat /home/jeason/projects/pythonStudy/MainForFirstCommit.py")
-print(result)
-"""
-输出：
-# encoding=utf-8
+if (comfirm == "n" or comfirm == "N"):
+    print(Const.EXIT_WITH_CANCEL)
+    exit()
 
-if __name__ == "__main__":
-    print("hello world")
-0
+# 拼凑配置字符串，回写配置
+newObjJsonStr = paramRange.getNewObjJsonStr()
 
-最后还带了一个0，表示主函数return 0这个返回值
+# 借助json进一步转义，以作为shell的字符串参数
+temp = json.dumps([newObjJsonStr])
+configCommandStr = Const.configCommandTemplate.format(temp[1:len(temp)])
 
-"""
+print("config command is: {}".format(configCommandStr))
 
+resultFile = None
+try:
+    resultFile = os.popen(configCommandStr)
+    resultStrArray = resultFile.readlines()
+    print("config command result:\n{}".format(resultStrArray))
 
-# f 是一个对象
-f = os.popen("cat /home/jeason/projects/pythonStudy/MainForFirstCommit.py")
-print(f)
-data = f.readlines()
-f.close()
-print(data)
-"""
-从f对象中readlines读取程序的标准输出，形成数组
-['# encoding=utf-8\n', '\n', 'if __name__ == "__main__":\n', '    print("hello world")\n']
-"""
+    if len(resultStrArray) > 0:
+        print(Const.EXIT_WITH_ERROR)
+    else:
+        print(Const.EXIT_WITH_SUCCESS)
 
-
+finally:
+    if resultFile:
+        resultFile.close()
